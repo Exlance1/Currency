b0ff20b1ef01ba1fda2f42f4d557dc09a9497bbf
Yasin <96873398+Exlance1@users.noreply.github.com>
2022-07-06 18:54:22 +0300

    Add project files.

---
+++ Curreny/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Curreny
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDolarSell = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblEuroBuy = new System.Windows.Forms.Label();
+            this.lblEuroSell = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblDollarbut = new System.Windows.Forms.Button();
+            this.lblDollarsellbut = new System.Windows.Forms.Button();
+            this.lblEurobuybt = new System.Windows.Forms.Button();
+            this.lblEurosellbt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtRemainder = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(536, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buy Dollars";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(536, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sell Dollars";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblDolarSell
+            // 
+            this.lblDolarSell.AutoSize = true;
+            this.lblDolarSell.BackColor = System.Drawing.Color.Transparent;
+            this.lblDolarSell.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDolarSell.ForeColor = System.Drawing.Color.Transparent;
+            this.lblDolarSell.Location = new System.Drawing.Point(713, 85);
+            this.lblDolarSell.Name = "lblDolarSell";
+            this.lblDolarSell.Size = new System.Drawing.Size(31, 32);
+            this.lblDolarSell.TabIndex = 3;
+            this.lblDolarSell.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(150, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Buy Euro";
+            // 
+            // lblEuroBuy
+            // 
+            this.lblEuroBuy.AutoSize = true;
+            this.lblEuroBuy.BackColor = System.Drawing.Color.Transparent;
+            this.lblEuroBuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEuroBuy.ForeColor = System.Drawing.Color.Transparent;
+            this.lblEuroBuy.Location = new System.Drawing.Point(294, 25);
+            this.lblEuroBuy.Name = "lblEuroBuy";
+            this.lblEuroBuy.Size = new System.Drawing.Size(31, 32);
+            this.lblEuroBuy.TabIndex = 5;
+            this.lblEuroBuy.Text = "0";
+            // 
+            // lblEuroSell
+            // 
+            this.lblEuroSell.AutoSize = true;
+            this.lblEuroSell.BackColor = System.Drawing.Color.Transparent;
+            this.lblEuroSell.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEuroSell.ForeColor = System.Drawing.Color.Transparent;
+            this.lblEuroSell.Location = new System.Drawing.Point(294, 75);
+            this.lblEuroSell.Name = "lblEuroSell";
+            this.lblEuroSell.Size = new System.Drawing.Size(31, 32);
+            this.lblEuroSell.TabIndex = 6;
+            this.lblEuroSell.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(150, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Sell Euro";
+            // 
+            // lblDollarbut
+            // 
+            this.lblDollarbut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDollarbut.Location = new System.Drawing.Point(482, 22);
+            this.lblDollarbut.Name = "lblDollarbut";
+            this.lblDollarbut.Size = new System.Drawing.Size(35, 32);
+            this.lblDollarbut.TabIndex = 8;
+            this.lblDollarbut.Text = "...";
+            this.lblDollarbut.UseVisualStyleBackColor = true;
+            this.lblDollarbut.Click += new System.EventHandler(this.lblDollarbut_Click);
+            // 
+            // lblDollarsellbut
+            // 
+            this.lblDollarsellbut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDollarsellbut.Location = new System.Drawing.Point(482, 78);
+            this.lblDollarsellbut.Name = "lblDollarsellbut";
+            this.lblDollarsellbut.Size = new System.Drawing.Size(35, 32);
+            this.lblDollarsellbut.TabIndex = 9;
+            this.lblDollarsellbut.Text = "...";
+            this.lblDollarsellbut.UseVisualStyleBackColor = true;
+            this.lblDollarsellbut.Click += new System.EventHandler(this.lblDollarsellbut_Click);
+            // 
+            // lblEurobuybt
+            // 
+            this.lblEurobuybt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEurobuybt.Location = new System.Drawing.Point(95, 22);
+            this.lblEurobuybt.Name = "lblEurobuybt";
+            this.lblEurobuybt.Size = new System.Drawing.Size(35, 32);
+            this.lblEurobuybt.TabIndex = 10;
+            this.lblEurobuybt.Text = "...";
+            this.lblEurobuybt.UseVisualStyleBackColor = true;
+            this.lblEurobuybt.Click += new System.EventHandler(this.lblEurobuybt_Click);
+            // 
+            // lblEurosellbt
+            // 
+            this.lblEurosellbt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEurosellbt.Location = new System.Drawing.Point(95, 75);
+            this.lblEurosellbt.Name = "lblEurosellbt";
+            this.lblEurosellbt.Size = new System.Drawing.Size(35, 32);
+            this.lblEurosellbt.TabIndex = 11;
+            this.lblEurosellbt.Text = "...";
+            this.lblEurosellbt.UseVisualStyleBackColor = true;
+            this.lblEurosellbt.Click += new System.EventHandler(this.lblEurosellbt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.txtRemainder);
+            this.groupBox1.Controls.Add(this.txtSum);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.txtQuantity);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.txtRate);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(482, 195);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 247);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(189, 199);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Process 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtRemainder
+            // 
+            this.txtRemainder.Location = new System.Drawing.Point(189, 160);
+            this.txtRemainder.Name = "txtRemainder";
+            this.txtRemainder.Size = new System.Drawing.Size(185, 22);
+            this.txtRemainder.TabIndex = 7;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(189, 115);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(185, 22);
+            this.txtSum.TabIndex = 6;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label12.Location = new System.Drawing.Point(18, 160);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(162, 32);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Remainder";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(189, 71);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(185, 22);
+            this.txtQuantity.TabIndex = 3;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label11.Location = new System.Drawing.Point(18, 115);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(75, 32);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Sum";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(189, 32);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(185, 22);
+            this.txtRate.TabIndex = 1;
+            this.txtRate.TextChanged += new System.EventHandler(this.txtRate_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label10.Location = new System.Drawing.Point(18, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 32);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Quantity";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(18, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 32);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Rate";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(713, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Process 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(889, 469);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblEurosellbt);
+            this.Controls.Add(this.lblEurobuybt);
+            this.Controls.Add(this.lblDollarsellbut);
+            this.Controls.Add(this.lblDollarbut);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblEuroSell);
+            this.Controls.Add(this.lblEuroBuy);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblDolarSell);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Currency Office";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDolarSell;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblEuroBuy;
+        private System.Windows.Forms.Label lblEuroSell;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button lblDollarbut;
+        private System.Windows.Forms.Button lblDollarsellbut;
+        private System.Windows.Forms.Button lblEurobuybt;
+        private System.Windows.Forms.Button lblEurosellbt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtRemainder;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Curreny/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace Curreny
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+     
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldocument = new XmlDocument();                          /*var tum degiskenleri icine alır int string*/
+            xmldocument.Load(bugun);
+
+            string dolarbuy = xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            label2.Text = dolarbuy;
+
+            string dolarsell = xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            lblDolarSell.Text = dolarsell;
+
+            string eurobuy = xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            lblEuroBuy.Text= eurobuy;
+
+            string eurosell = xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            lblEuroSell.Text = eurosell;
+
+
+        }
+
+        private void lblDollarbut_Click(object sender, EventArgs e)
+        {
+            txtRate.Text = label2.Text;
+        }
+
+        private void lblDollarsellbut_Click(object sender, EventArgs e)
+        {
+            txtRate.Text = lblDolarSell.Text;
+        }
+
+        private void lblEurobuybt_Click(object sender, EventArgs e)
+        {
+            txtRate.Text = lblEuroBuy.Text;
+        }
+
+        private void lblEurosellbt_Click(object sender, EventArgs e)
+        {
+            txtRate.Text = lblEuroSell.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double rate, quantity, sum;
+            rate=Convert.ToDouble(txtRate.Text);
+            quantity = Convert.ToDouble(txtQuantity.Text);
+            sum = rate * quantity;
+            txtSum.Text = sum.ToString();
+
+
+
+        }
+
+        private void txtRate_TextChanged(object sender, EventArgs e)
+        {
+            txtRate.Text=txtRate.Text.Replace(".",",");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           double rate= Convert.ToDouble(txtRate.Text);
+           double quantity=Convert.ToInt32(txtQuantity.Text);
+           double sum =Convert.ToInt32(quantity / rate);
+           txtSum.Text = sum.ToString();
+           double remainder1;
+           
+           double sum2 = Convert.ToDouble(quantity / rate);
+           remainder1 = sum - sum2;
+           txtRemainder.Text = remainder1.ToString();
+           
+
+
+
+        }
+    }
+}
